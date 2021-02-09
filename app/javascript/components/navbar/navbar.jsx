75aeeda3c993b36f1282b854243c7bc5d07b5474
Nathanstuff <n.odonnell@hotmail.com>
2021-02-09 16:29:25 +1100

    fixed import error

--- app/javascript/components/navbar/navbar.jsx
+++ app/javascript/components/navbar/navbar.jsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import logo from 'images/logo.svg';
+import logo from '../../../assets/images/logo.svg';
 import { auth } from '../../firebase/firebase';
 import { useHistory } from 'react-router-dom';
 
